0e8391b4f5561f0f6747203444d92819dae235f2
yuichi <yuichi@kanuhura>
2013-08-20 21:26:00 +0900

    importを整理

--- src/hello/HelloViewController.java
+++ src/hello/HelloViewController.java
@@ -2,13 +2,11 @@ package hello;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
 
 public class HelloViewController implements Initializable {
     @FXML
