7b864449ad2403492b9645dce3de93607de99f0b
yuichi <yuichi@kanuhura>
2013-08-20 19:57:06 +0900

    Fist Commit

---
+++ src/hello/Hello.java
@@ -0,0 +1,25 @@
+package hello;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Hello extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("HelloView.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
