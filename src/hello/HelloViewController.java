96292c0070f588155bfff5aa3849b732e643c18b
yuichi <yuichi@kanuhura>
2013-08-20 20:14:15 +0900

    FXMLとコントローラのバインディング

--- src/hello/HelloViewController.java
+++ src/hello/HelloViewController.java
@@ -6,8 +6,18 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloViewController implements Initializable {
+    @FXML
+    private TextField textField;
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handle(ActionEvent event) {
+        
+    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
