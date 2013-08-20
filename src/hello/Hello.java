4f11f783a621c6162f37fa649fdf3c598e495533
yuichi <yuichi@kanuhura>
2013-08-20 20:30:26 +0900

    テキストフィールドの横幅をコンテナにバインドする

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -2,18 +2,19 @@ package hello;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Hello extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("HelloView.fxml"));
+        VBox root = FXMLLoader.load(getClass().getResource("HelloView.fxml"));
         
         Scene scene = new Scene(root);
         
+        stage.setTitle("Hello, World!");
         stage.setScene(scene);
         stage.show();
     }
--- src/hello/HelloViewController.java
+++ src/hello/HelloViewController.java
@@ -2,13 +2,17 @@ package hello;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 public class HelloViewController implements Initializable {
+    @FXML
+    private VBox root;
     @FXML
     private TextField textField;
     @FXML
@@ -21,7 +25,7 @@ public class HelloViewController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        textField.maxWidthProperty().bind(Bindings.multiply(0.8, root.widthProperty()));
     }    
     
 }
